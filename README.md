e2591fea7134062a01969cb2f5edf16e4aa27dbf
Pranjal Jain <pranjaljain0697@gmail.com>
2020-04-05 05:59:10 +0530

    minor changes

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # DaShop
+
+DaShop is built using React Native, This applicaton facilitates 8+ screens equipped with full functionality that an eCommerce app requires.This is a great starting point for app development
+
+# Screens
--- components/Profile.js
+++ components/Profile.js
@@ -1,33 +1,73 @@
 import React, { useState, useEffect } from 'react'
-import { StyleSheet, Text, View, Button } from 'react-native'
+import { StyleSheet, Text, View, Button, Image } from 'react-native'
 
 const Profile = () => {
 
     // https://randomuser.me/api/
     const [isLoading, setLoading] = useState(true);
     const [data, setData] = useState([]);
+    const [name, setName] = useState([]);
+    const [userPhoto, setUserPhoto] = useState([]);
+    const [email, setEmail] = useState([]);
+    const [city, setCity] = useState([]);
+    const [country, setCountry] = useState([]);
 
     // const [products, setproducts] = useState(getMoviesFromApi())
     useEffect(() => {
+        // loadDataOnlyOnce();
         fetch('https://randomuser.me/api/')
           .then((response) => response.json())
-          .then((json) => setData(json.results))
+          .then((json) => {
+              setData((json.results)[0])
+              setName((((json.results)[0])['name'])['first']+` `+(((json.results)[0])['name'])['last'])
+              setUserPhoto((((json.results)[0])['picture'])['large'])
+              setEmail(((json.results)[0])['email'])
+              setCity((((json.results)[0])['location'])['city'])
+              setCountry((((json.results)[0])['location'])['country'])
+
+        })
           .catch((error) => console.error(error))
           .finally(() => setLoading(false));
-      });
-    
-      var name=((data[0])['name'])['first']+` `+((data[0])['name'])['last']
+      },[]);
+      var objData=data[0]
+
+    const onPressTester=()=>{
+        console.log(name)
+    }
 
     return (
         <View>
-            <Button
-                title="Click"
-                onPress={()=>onPressHandler()}
-            />
+            <Image source = {{ uri: userPhoto }} style={styles.userImage} />
+            <View style={styles.userContent}>
+                <Text style={styles.userName}>Name : {name}</Text>
+                <Text style={styles.userEmail}>Email : {email}</Text>
+                <Text style={styles.userLocation}>City : {city}</Text>
+                <Text style={styles.userLocation}>Country : {country}</Text>
+            </View>
         </View>
     )
 }
 
 export default Profile
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    userImage: {
+        height: 400,
+    },
+    userContent:{
+        flex:1,
+        marginTop:30,
+    },
+    userName:{
+        marginHorizontal:20,
+        fontSize:22,
+    },
+    userEmail:{
+        marginHorizontal:20,
+        fontSize:22,
+    },
+    userLocation:{
+        marginHorizontal:20,
+        fontSize:22,
+    }
+})
